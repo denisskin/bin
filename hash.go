afbe7e5fe89cca63a61a1c2018370065fa931e94
denisskin <denis@begom.com>
2017-07-06 13:48:22 +0300

    improve Hash function

--- hash.go
+++ hash.go
@@ -1,9 +1,6 @@
 package bin
 
-import (
-	"crypto/sha256"
-	"io"
-)
+import "crypto/sha256"
 
 func Hash32(values ...interface{}) uint32 {
 	h256 := Hash256(values...)
@@ -27,47 +24,9 @@ func Hash160(values ...interface{}) []byte {
 
 func Hash256(values ...interface{}) []byte {
 	hash := sha256.New()
-
+	w := NewWriter(hash)
 	for _, val := range values {
-		switch v := val.(type) {
-
-		case []byte:
-			hash.Write(v)
-
-		case string:
-			hash.Write([]byte(v))
-
-		case int:
-			hash.Write(Uint64ToBytes(uint64(v)))
-
-		case uint:
-			hash.Write(Uint64ToBytes(uint64(v)))
-
-		case int64:
-			hash.Write(Uint64ToBytes(uint64(v)))
-
-		case uint64:
-			hash.Write(Uint64ToBytes(v))
-
-		case int32:
-			hash.Write(Uint32ToBytes(uint32(v)))
-
-		case uint32:
-			hash.Write(Uint32ToBytes(v))
-
-		case error:
-			hash.Write([]byte(v.Error()))
-
-		case BinEncoder:
-			buf, _ := Encode(v)
-			hash.Write(buf)
-
-		case io.Reader:
-			io.Copy(hash, v)
-
-		default:
-			panic("SHA256 Not expected value")
-		}
+		w.WriteVar(val)
 	}
 	return hash.Sum(nil)
 }
